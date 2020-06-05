8ed6323b5a6f59edfbfa014705ee6430a7bb2859
namdongvando <32663464+namdongvando@users.noreply.github.com>
2020-06-05 09:50:55 +0700

    Revert "Revert "Timer""
    This reverts commit 0bac8bba3641399cce45bff3c74240daec43a69c.

---
+++ UngDung1/DesktopApp1/FormMain.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace DesktopApp1
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip2 = new System.Windows.Forms.MenuStrip();
+            this.hinhHocToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.hinhToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.hìnhVuôngToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.hìnhTrònToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.làmViệcVớiThoiGianToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.formTimerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip2
+            // 
+            this.menuStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.hinhHocToolStripMenuItem,
+            this.làmViệcVớiThoiGianToolStripMenuItem});
+            this.menuStrip2.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip2.Name = "menuStrip2";
+            this.menuStrip2.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip2.TabIndex = 1;
+            this.menuStrip2.Text = "menuStrip2";
+            // 
+            // hinhHocToolStripMenuItem
+            // 
+            this.hinhHocToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.hinhToolStripMenuItem,
+            this.hìnhVuôngToolStripMenuItem,
+            this.hìnhTrònToolStripMenuItem});
+            this.hinhHocToolStripMenuItem.Name = "hinhHocToolStripMenuItem";
+            this.hinhHocToolStripMenuItem.Size = new System.Drawing.Size(70, 20);
+            this.hinhHocToolStripMenuItem.Text = "Hinh Hoc";
+            // 
+            // hinhToolStripMenuItem
+            // 
+            this.hinhToolStripMenuItem.Name = "hinhToolStripMenuItem";
+            this.hinhToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.hinhToolStripMenuItem.Text = "Hình Tam Giác";
+            this.hinhToolStripMenuItem.Click += new System.EventHandler(this.hinhToolStripMenuItem_Click);
+            // 
+            // hìnhVuôngToolStripMenuItem
+            // 
+            this.hìnhVuôngToolStripMenuItem.Name = "hìnhVuôngToolStripMenuItem";
+            this.hìnhVuôngToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.hìnhVuôngToolStripMenuItem.Text = "Hình vuông";
+            this.hìnhVuôngToolStripMenuItem.Click += new System.EventHandler(this.hìnhVuôngToolStripMenuItem_Click);
+            // 
+            // hìnhTrònToolStripMenuItem
+            // 
+            this.hìnhTrònToolStripMenuItem.Name = "hìnhTrònToolStripMenuItem";
+            this.hìnhTrònToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.hìnhTrònToolStripMenuItem.Text = "Hình Tròn";
+            this.hìnhTrònToolStripMenuItem.Click += new System.EventHandler(this.hìnhTrònToolStripMenuItem_Click);
+            // 
+            // làmViệcVớiThoiGianToolStripMenuItem
+            // 
+            this.làmViệcVớiThoiGianToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.formTimerToolStripMenuItem});
+            this.làmViệcVớiThoiGianToolStripMenuItem.Name = "làmViệcVớiThoiGianToolStripMenuItem";
+            this.làmViệcVớiThoiGianToolStripMenuItem.Size = new System.Drawing.Size(141, 20);
+            this.làmViệcVớiThoiGianToolStripMenuItem.Text = "Làm Việc Với Thoi Gian";
+            // 
+            // formTimerToolStripMenuItem
+            // 
+            this.formTimerToolStripMenuItem.Name = "formTimerToolStripMenuItem";
+            this.formTimerToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.formTimerToolStripMenuItem.Text = "FormTimer";
+            this.formTimerToolStripMenuItem.Click += new System.EventHandler(this.formTimerToolStripMenuItem_Click);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip2);
+            this.IsMdiContainer = true;
+            this.Name = "FormMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FormMain";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.menuStrip2.ResumeLayout(false);
+            this.menuStrip2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.MenuStrip menuStrip2;
+        private System.Windows.Forms.ToolStripMenuItem hinhHocToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem hinhToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem hìnhVuôngToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem hìnhTrònToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem làmViệcVớiThoiGianToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem formTimerToolStripMenuItem;
+    }
+}
---
+++ UngDung1/DesktopApp1/FormTimer.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace DesktopApp1
+{
+    partial class FormTimer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pbxMario = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pbxMario1 = new System.Windows.Forms.PictureBox();
+            this.pbxMario2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxMario)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxMario1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxMario2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbxMario
+            // 
+            this.pbxMario.Image = global::DesktopApp1.Properties.Resources.giphy1;
+            this.pbxMario.Location = new System.Drawing.Point(12, 118);
+            this.pbxMario.Name = "pbxMario";
+            this.pbxMario.Size = new System.Drawing.Size(58, 54);
+            this.pbxMario.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbxMario.TabIndex = 0;
+            this.pbxMario.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pbxMario1
+            // 
+            this.pbxMario1.Image = global::DesktopApp1.Properties.Resources.giphy1;
+            this.pbxMario1.Location = new System.Drawing.Point(720, 118);
+            this.pbxMario1.Name = "pbxMario1";
+            this.pbxMario1.Size = new System.Drawing.Size(58, 54);
+            this.pbxMario1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbxMario1.TabIndex = 1;
+            this.pbxMario1.TabStop = false;
+            // 
+            // pbxMario2
+            // 
+            this.pbxMario2.Image = global::DesktopApp1.Properties.Resources.giphy1;
+            this.pbxMario2.Location = new System.Drawing.Point(720, 210);
+            this.pbxMario2.Name = "pbxMario2";
+            this.pbxMario2.Size = new System.Drawing.Size(58, 54);
+            this.pbxMario2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbxMario2.TabIndex = 2;
+            this.pbxMario2.TabStop = false;
+            // 
+            // FormTimer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pbxMario2);
+            this.Controls.Add(this.pbxMario1);
+            this.Controls.Add(this.pbxMario);
+            this.Name = "FormTimer";
+            this.Text = "FormTimer";
+            this.Load += new System.EventHandler(this.FormTimer_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.FormTimer_KeyDown);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.FormTimer_KeyPress);
+            ((System.ComponentModel.ISupportInitialize)(this.pbxMario)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxMario1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxMario2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbxMario;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pbxMario1;
+        private System.Windows.Forms.PictureBox pbxMario2;
+    }
+}
